5d787ea47b8a225092f04a5870d0e879cef3efb4
Dustin Smith <dustin.smith@utexas.edu>
2012-03-06 14:14:29 -0600

    Update NewLineSep.scala

--- NewLineSep.scala
+++ NewLineSep.scala
@@ -11,8 +11,8 @@ import java.io._
 
 val text = io.Source.fromFile(args(0)).getLines.map(line => line.dropRight(1)).toList //grab contents of file from CL argument, then grab each line,
 																					 // then drop off the comma and convert to list
-var out_file = new java.io.FileOutputStream(args(1))  // Java for getting the output file path
-var out_stream = new java.io.PrintStream(out_file)   // more java for outputing the contents to a file
+var out_file = new java.io.FileOutputStream(args(1))  
+var out_stream = new java.io.PrintStream(out_file)   
 
 text.foreach(x => out_stream.print(x+",")) // For each item in the List, print the item + a "," for seperator.
-out_stream.close // close output stream (Java)
+out_stream.close
